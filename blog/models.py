ddc7cc377d5e0cc2bcdbad4872798d00313d5ea0
SergioLucas1 <sergionhs@gmail.com>
2022-02-01 17:22:06 -0300

    Criação do modelo Post

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    conteudo = models.TextField()
+    STATUS = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
+    publicado = models.DateTimeField(default=timezone.now)
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS, default='rascunho')
+
+    def __str__(self):
+        return self.titulo
